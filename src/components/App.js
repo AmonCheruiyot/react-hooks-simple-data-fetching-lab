c1a5ba37b54509608149daed7bdfd06f5f7e16ba
AmonCheruiyot <161847144+AmonCheruiyot@users.noreply.github.com>
2024-04-30 20:37:37 +0300

    CompletesTests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    const [dogImageUrl, setDogImageUrl] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {
+                setDogImageUrl(data.message);
+                setLoading(false);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <img src={dogImageUrl} alt="A Random Dog" />
+            )}
+        </div>
+    );
+};
+
+export default App;
